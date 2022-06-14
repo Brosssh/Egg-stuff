0e77ec84a3b3528c6247ba5b00ac04ca50679d8d
Mattia_Broch <tiabroch@gmail.com>
2022-06-14 17:17:36 +0200

    Credit to effi

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ while checksum==0:
         print("Insert valid EID (EI12345678)\n")
 
 #print("Total legendaries: "+str(get_leg_number_total(result)))
-print("Hello "+result.backup.user_name+" glad to see you here")
+print("\nHello "+result.backup.user_name+", glad to see you here")
 
 print("\nTotal drops from exthens: "+ str(get_total_drop_exthens(result)))
 
@@ -31,4 +31,7 @@ print("\nTotal extended henerprise: "+str(get_total_ships_exthens(result)))
 print("Total standard henerprise: "+str(get_total_ships_standhens(result)))
 print("Total short henerprise: "+str(get_total_ships_shens(result)))
 
+print("\nCredit to EffectiveMess#0256 for the formula\n")
 print("\nLLC: "+str(formula_LLC(get_leg_number_total(result),get_total_craft(result),get_total_ships_exthens(result),get_total_ships_standhens(result),get_total_ships_shens(result))))
+
+print("\nHave a good day (:")
