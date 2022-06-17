4e9ce9a9348bee9d5233b3ae4e906953a875d7a1
TiaBroch <tiabroch@gmail.com>
2022-06-17 17:28:06 +0200

    Added a quick table to show leaderboard

---
+++ leaderboardShips/new_EID.py
@@ -0,0 +1,65 @@
+from leaderboardShips.ships_functions import *
+from server_manager import server
+from utiliy import encrypt_string
+import os
+
+def insert_EID(mongo):
+    checksum=0
+
+    while checksum==0:
+        try:
+            EID = str(input("Please enter your EID: "))
+            server_manager = server()
+            server_manager.set_EID(EID)
+            result = server_manager.execute_call()
+            checksum=result.backup.checksum
+            if checksum==0:
+                print("EID not registered on Egg Inc server\n")
+            else:
+                break
+        except:
+            print("Insert valid EID (EI12345678)\n")
+
+    loot_dict=loots(result,server_manager)
+
+    user_name=result.backup.user_name
+
+    dict_loots=semplify_dict(loot_dict)
+
+    encryptedEID = encrypt_string(EID)
+
+    final_dict={"EID":encryptedEID,"name":user_name,"ships":dict_loots}
+
+    #conn=os.getenv("mongo_conn")
+
+    '''
+    
+    with open('D:\\ship_json.txt') as loot_json:
+        loot_dict = json.load(loot_json)
+        loots=semplify_dict(loot_dict)
+    
+    encryptedEID = encrypt_string("test_eid")
+    final_dict={"EID":encryptedEID,"name":"Q","loots":loots}
+    
+    '''
+
+    #ships that weren't on the db before this execution, will be modified below
+    new_ships=final_dict
+
+
+    if not mongo.user_exists(encryptedEID):
+        mongo.insert_full_user_ships(final_dict)
+        print("Data successfully loaded!")
+    else:
+        new_ships=mongo.update_and_return_user_ships(final_dict,encryptedEID)
+        if new_ships is not None:
+            print("Data successfully updated!")
+        else:
+            print("No new ships")
+
+    if new_ships is not None:
+        leaderboard_dict = mongo.get_leaderboard()
+
+        leaderboard_updated=update_leaderboard(leaderboard_dict,new_ships,3)
+        mongo.load_updated_leaderboard(leaderboard_updated)
+
