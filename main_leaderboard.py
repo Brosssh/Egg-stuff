10ef65876b6d0ff59260599e1206497511ce3dd1
Mattia_Broch <tiabroch@gmail.com>
2022-06-27 22:38:52 +0200

    Fixed append of new ships

--- main_leaderboard.py
+++ main_leaderboard.py
@@ -4,11 +4,13 @@ from leaderboardShips.show_leaderboard import show_leader
 
 print("This is a very test")
 
-lines = open("D:/mongo_cred.txt", "r").read().split('\n')
-user = lines[0]
-pssw = lines[1]
+#lines = open("D:/mongo_cred.txt", "r").read().split('\n')
+#user = lines[0]
+#pssw = lines[1]
 
-conn = "mongodb+srv://" + user + ":" + pssw + "@eggcluster.sbrsi.mongodb.net/?retryWrites=true&w=majority"
+#conn = "mongodb+srv://" + user + ":" + pssw + "@eggcluster.sbrsi.mongodb.net/?retryWrites=true&w=majority"
+
+conn="mongodb+srv://Ro0t:yfPVFbCPWhs7rCvW@eggcluster.sbrsi.mongodb.net/?retryWrites=true&w=majority"
 mongo = mongo_manager(conn)
 
 try:
